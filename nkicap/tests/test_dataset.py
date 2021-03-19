538b12d6e2cb20d21bc681523d9de2cc445ccfb3
Hao-Ting Wang <htwangtw@gmail.com>
2021-03-19 14:34:52 +0000

    TEST/FIX fix dataset test

--- nkicap/tests/test_dataset.py
+++ nkicap/tests/test_dataset.py
@@ -7,7 +7,7 @@ def test_fetch_dataset():
     assert len(dataset["participant_id"]) == 711
     assert len(dataset["subject_cap_map"]) == 711
     assert len(dataset["roi_cap_map"]) == 1054
-    assert dataset["group_cap_map"].shape == (1054, 8)
+    assert len(dataset["group_cap_map"]) == 8
     assert dataset["cap_occurence"].shape == (711, 8)
     assert dataset["cap_duration"].shape == (711, 8)
-    assert dataset["subject_cap_map"]["A00027544"].shape == (1054, 8)
+    assert len(dataset["subject_cap_map"]["A00027544"]) == 8
