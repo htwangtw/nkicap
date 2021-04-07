459aed16574e303ca7240b854d4699ea060bd51b
Hao-Ting Wang <htwangtw@gmail.com>
2021-04-07 17:41:32 +0100

    Revert "ADD detailed description of this project (#8)"
    This reverts commit a129ba7c389cc7b10911c50c1a631d5a8857846c.

--- nkicap/__init__.py
+++ nkicap/__init__.py
@@ -5,4 +5,4 @@ Data analysis suit of the CAP project
 """
 from .utils import load_data
 
-__all__ = [load_data, "__version__"]  # not allowing scripts to be read
+__all__ = [load_data, "__version__"]
--- nkicap/make_dataset.py
+++ nkicap/make_dataset.py
@@ -1,5 +1,5 @@
 """
-Get CAP data and MRIQ of the current sample.
+parse dataset
 """
 from pathlib import Path
 import pandas as pd
