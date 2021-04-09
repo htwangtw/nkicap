b6e87901814ba646019b386afa6f2821173250d9
Hao-Ting Wang <htwangtw@gmail.com>
2021-04-09 17:21:57 +0100

    RF/ADD improve data loading module and new analysis
    Address JS's concern of conceptually duplicating mriq
    * New `Data` class for easy data filtering
    * Add result removing dulicated questions

--- nkicap/tests/test_utils.py
+++ nkicap/tests/test_utils.py
@@ -1,19 +1,22 @@
-from ..utils import load_data, get_project_path, read_tsv
+from ..utils import Data, get_project_path, read_tsv
 from .utils import get_test_data_path
 
 import pytest
 from pathlib import Path
 
 testdata = Path(get_test_data_path()) / "file.tsv"
+testmriq = Path(get_test_data_path()) / "mriq.tsv"
+
 testcsv = Path(get_test_data_path()) / "file_csv.csv"
 
 
 def test_load_data():
-    d = load_data("mriq", datapath=testdata)
-    assert d.shape[1] == 3
-    d = load_data(datapath=testdata)
-    assert d.shape[1] == 13
-    pytest.raises(KeyError, load_data, "blah", datapath=testdata)
+    d = Data(datapath=testdata, mriq_label=testmriq)
+    m = d.load("mriq_")
+    assert m.shape[1] == 3
+    m = d.load()
+    assert m.shape[1] == 13
+    pytest.raises(KeyError, d.load, keyword="blah")
 
 
 def test_get_project_path():
